f1e0d4b8c0f97e66309695dba470671bff5d44bf
James A. Petts <jamesapetts@gmail.com>
2020-10-28 11:35:40 +0000

    Test

--- src/rendering/renderGrayscaleImage.js
+++ src/rendering/renderGrayscaleImage.js
@@ -1,12 +1,12 @@
-import storedPixelDataToCanvasImageData from '../internal/storedPixelDataToCanvasImageData.js';
-import storedPixelDataToCanvasImageDataRGBA from '../internal/storedPixelDataToCanvasImageDataRGBA.js';
-import setToPixelCoordinateSystem from '../setToPixelCoordinateSystem.js';
-import now from '../internal/now.js';
-import webGL from '../webgl/index.js';
-import getLut from './getLut.js';
-import doesImageNeedToBeRendered from './doesImageNeedToBeRendered.js';
-import initializeRenderCanvas from './initializeRenderCanvas.js';
-import saveLastRendered from './saveLastRendered.js';
+import storedPixelDataToCanvasImageData from "../internal/storedPixelDataToCanvasImageData.js";
+import storedPixelDataToCanvasImageDataRGBA from "../internal/storedPixelDataToCanvasImageDataRGBA.js";
+import setToPixelCoordinateSystem from "../setToPixelCoordinateSystem.js";
+import now from "../internal/now.js";
+import webGL from "../webgl/index.js";
+import getLut from "./getLut.js";
+import doesImageNeedToBeRendered from "./doesImageNeedToBeRendered.js";
+import initializeRenderCanvas from "./initializeRenderCanvas.js";
+import saveLastRendered from "./saveLastRendered.js";
 
 /**
  * Returns an appropriate canvas to render the Image. If the canvas available in the cache is appropriate
@@ -19,49 +19,64 @@ import saveLastRendered from './saveLastRendered.js';
  * @returns {HTMLCanvasElement} An appropriate canvas for rendering the image
  * @memberof rendering
  */
-function getRenderCanvas (enabledElement, image, invalidated, useAlphaChannel = true) {
-  const canvasWasColor = enabledElement.renderingTools.lastRenderedIsColor === true;
+function getRenderCanvas(
+    enabledElement,
+    image,
+    invalidated,
+    useAlphaChannel = true
+) {
+    const canvasWasColor =
+        enabledElement.renderingTools.lastRenderedIsColor === true;
 
-  if (!enabledElement.renderingTools.renderCanvas || canvasWasColor) {
-    enabledElement.renderingTools.renderCanvas = document.createElement('canvas');
-    initializeRenderCanvas(enabledElement, image);
-  }
+    if (!enabledElement.renderingTools.renderCanvas || canvasWasColor) {
+        enabledElement.renderingTools.renderCanvas = document.createElement(
+            "canvas"
+        );
+        initializeRenderCanvas(enabledElement, image);
+    }
 
-  const renderCanvas = enabledElement.renderingTools.renderCanvas;
+    const renderCanvas = enabledElement.renderingTools.renderCanvas;
 
-  if (doesImageNeedToBeRendered(enabledElement, image) === false && invalidated !== true) {
+    if (
+        doesImageNeedToBeRendered(enabledElement, image) === false &&
+        invalidated !== true
+    ) {
+        return renderCanvas;
+    }
+
+    // If our render canvas does not match the size of this image reset it
+    // NOTE: This might be inefficient if we are updating multiple images of different
+    // Sizes frequently.
+    if (
+        renderCanvas.width !== image.width ||
+        renderCanvas.height !== image.height
+    ) {
+        initializeRenderCanvas(enabledElement, image);
+    }
+
+    // Get the lut to use
+    let start = now();
+    const lut = getLut(image, enabledElement.viewport, invalidated);
+
+    image.stats = image.stats || {};
+    image.stats.lastLutGenerateTime = now() - start;
+
+    const renderCanvasData = enabledElement.renderingTools.renderCanvasData;
+    const renderCanvasContext =
+        enabledElement.renderingTools.renderCanvasContext;
+
+    // Gray scale image - apply the lut and put the resulting image onto the render canvas
+    if (useAlphaChannel) {
+        storedPixelDataToCanvasImageData(image, lut, renderCanvasData.data);
+    } else {
+        storedPixelDataToCanvasImageDataRGBA(image, lut, renderCanvasData.data);
+    }
+
+    start = now();
+    renderCanvasContext.putImageData(renderCanvasData, 0, 0);
+    image.stats.lastPutImageDataTime = now() - start;
+
     return renderCanvas;
-  }
-
-  // If our render canvas does not match the size of this image reset it
-  // NOTE: This might be inefficient if we are updating multiple images of different
-  // Sizes frequently.
-  if (renderCanvas.width !== image.width || renderCanvas.height !== image.height) {
-    initializeRenderCanvas(enabledElement, image);
-  }
-
-  // Get the lut to use
-  let start = now();
-  const lut = getLut(image, enabledElement.viewport, invalidated);
-
-  image.stats = image.stats || {};
-  image.stats.lastLutGenerateTime = now() - start;
-
-  const renderCanvasData = enabledElement.renderingTools.renderCanvasData;
-  const renderCanvasContext = enabledElement.renderingTools.renderCanvasContext;
-
-  // Gray scale image - apply the lut and put the resulting image onto the render canvas
-  if (useAlphaChannel) {
-    storedPixelDataToCanvasImageData(image, lut, renderCanvasData.data);
-  } else {
-    storedPixelDataToCanvasImageDataRGBA(image, lut, renderCanvasData.data);
-  }
-
-  start = now();
-  renderCanvasContext.putImageData(renderCanvasData, 0, 0);
-  image.stats.lastPutImageDataTime = now() - start;
-
-  return renderCanvas;
 }
 
 /**
@@ -72,54 +87,66 @@ function getRenderCanvas (enabledElement, image, invalidated, useAlphaChannel = 
  * @returns {void}
  * @memberof rendering
  */
-export function renderGrayscaleImage (enabledElement, invalidated) {
-  if (enabledElement === undefined) {
-    throw new Error('drawImage: enabledElement parameter must not be undefined');
-  }
+export function renderGrayscaleImage(enabledElement, invalidated) {
+    if (enabledElement === undefined) {
+        throw new Error(
+            "drawImage: enabledElement parameter must not be undefined"
+        );
+    }
 
-  const image = enabledElement.image;
+    const image = enabledElement.image;
 
-  if (image === undefined) {
-    throw new Error('drawImage: image must be loaded before it can be drawn');
-  }
+    if (image === undefined) {
+        throw new Error(
+            "drawImage: image must be loaded before it can be drawn"
+        );
+    }
 
-  // Get the canvas context and reset the transform
-  const context = enabledElement.canvas.getContext('2d');
+    // Get the canvas context and reset the transform
+    const context = enabledElement.canvas.getContext("2d");
 
-  context.setTransform(1, 0, 0, 1, 0, 0);
+    context.setTransform(1, 0, 0, 1, 0, 0);
 
-  // Clear the canvas
-  context.fillStyle = 'black';
-  context.fillRect(0, 0, enabledElement.canvas.width, enabledElement.canvas.height);
+    // Clear the canvas
+    context.fillStyle = "black";
+    context.fillRect(
+        0,
+        0,
+        enabledElement.canvas.width,
+        enabledElement.canvas.height
+    );
 
-  // Turn off image smooth/interpolation if pixelReplication is set in the viewport
-  context.imageSmoothingEnabled = !enabledElement.viewport.pixelReplication;
-  context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
+    // Turn off image smooth/interpolation if pixelReplication is set in the viewport
+    context.imageSmoothingEnabled = !enabledElement.viewport.pixelReplication;
+    context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
 
-  // Save the canvas context state and apply the viewport properties
-  setToPixelCoordinateSystem(enabledElement, context);
+    // Save the canvas context state and apply the viewport properties
+    setToPixelCoordinateSystem(enabledElement, context);
 
-  let renderCanvas;
+    let renderCanvas;
 
-  if (enabledElement.options && enabledElement.options.renderer &&
-    enabledElement.options.renderer.toLowerCase() === 'webgl') {
-    // If this enabled element has the option set for WebGL, we should
-    // User it as our renderer.
-    renderCanvas = webGL.renderer.render(enabledElement);
-  } else {
-    // If no options are set we will retrieve the renderCanvas through the
-    // Normal Canvas rendering path
-    renderCanvas = getRenderCanvas(enabledElement, image, invalidated);
-  }
+    if (
+        enabledElement.options &&
+        enabledElement.options.renderer &&
+        enabledElement.options.renderer.toLowerCase() === "webgl"
+    ) {
+        // If this enabled element has the option set for WebGL, we should
+        // User it as our renderer.
+        renderCanvas = webGL.renderer.render(enabledElement);
+    } else {
+        // If no options are set we will retrieve the renderCanvas through the
+        // Normal Canvas rendering path
+        renderCanvas = getRenderCanvas(enabledElement, image, invalidated);
+    }
 
-  const sx = enabledElement.viewport.displayedArea.tlhc.x - 1;
-  const sy = enabledElement.viewport.displayedArea.tlhc.y - 1;
-  const width = enabledElement.viewport.displayedArea.brhc.x - sx;
-  const height = enabledElement.viewport.displayedArea.brhc.y - sy;
+    const sx = enabledElement.viewport.displayedArea.tlhc.x - 1;
+    const sy = enabledElement.viewport.displayedArea.tlhc.y - 1;
+    const width = enabledElement.viewport.displayedArea.brhc.x - sx;
+    const height = enabledElement.viewport.displayedArea.brhc.y - sy;
 
-  context.drawImage(renderCanvas, sx, sy, width, height, 0, 0, width, height);
+    context.drawImage(renderCanvas, sx, sy, width, height, 0, 0, width, height);
 
-  enabledElement.renderingTools = saveLastRendered(enabledElement);
+    enabledElement.renderingTools = saveLastRendered(enabledElement);
 }
 
 /**
@@ -131,24 +158,28 @@ export function renderGrayscaleImage (enabledElement, invalidated) {
                                         This does not work if this layer is not the first layer in the enabledElement.
  * @returns {void}
  */
-export function addGrayscaleLayer (layer, invalidated, useAlphaChannel = false) {
-  if (layer === undefined) {
-    throw new Error('addGrayscaleLayer: layer parameter must not be undefined');
-  }
+export function addGrayscaleLayer(layer, invalidated, useAlphaChannel = false) {
+    if (layer === undefined) {
+        throw new Error(
+            "addGrayscaleLayer: layer parameter must not be undefined"
+        );
+    }
 
-  const image = layer.image;
+    const image = layer.image;
 
-  if (image === undefined) {
-    throw new Error('addGrayscaleLayer: image must be loaded before it can be drawn');
-  }
+    if (image === undefined) {
+        throw new Error(
+            "addGrayscaleLayer: image must be loaded before it can be drawn"
+        );
+    }
 
-  layer.canvas = getRenderCanvas(layer, image, invalidated, useAlphaChannel);
+    layer.canvas = getRenderCanvas(layer, image, invalidated, useAlphaChannel);
 
-  const context = layer.canvas.getContext('2d');
+    const context = layer.canvas.getContext("2d");
 
-  // Turn off image smooth/interpolation if pixelReplication is set in the viewport
-  context.imageSmoothingEnabled = !layer.viewport.pixelReplication;
-  context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
+    // Turn off image smooth/interpolation if pixelReplication is set in the viewport
+    context.imageSmoothingEnabled = !layer.viewport.pixelReplication;
+    context.mozImageSmoothingEnabled = context.imageSmoothingEnabled;
 
-  layer.renderingTools = saveLastRendered(layer);
+    layer.renderingTools = saveLastRendered(layer);
 }
--- src/setViewport.js
+++ src/setViewport.js
@@ -1,6 +1,6 @@
-import getDefaultViewport from './internal/getDefaultViewport.js';
-import { getEnabledElement } from './enabledElements.js';
-import updateImage from './updateImage.js';
+import getDefaultViewport from "./internal/getDefaultViewport.js";
+import { getEnabledElement } from "./enabledElements.js";
+import updateImage from "./updateImage.js";
 
 const MIN_WINDOW_WIDTH = 0.000001;
 const MIN_VIEWPORT_SCALE = 0.0001;
@@ -13,44 +13,49 @@ const MIN_VIEWPORT_SCALE = 0.0001;
  * @returns {void}
  * @memberof ViewportSettings
  */
-export default function (element, viewport) {
-  const enabledElement = getEnabledElement(element);
+export default function(element, viewport) {
+    const enabledElement = getEnabledElement(element);
 
-  // If viewport is not already set, start with default and merge new
-  // viewport options later
-  if (enabledElement.viewport === undefined) {
-    enabledElement.viewport = getDefaultViewport(enabledElement.canvas);
-  }
-
-  // Merge viewport
-  if (viewport) {
-    for (const attrname in viewport) {
-      if (viewport[attrname] !== null) {
-        enabledElement.viewport[attrname] = viewport[attrname];
-      }
+    // If viewport is not already set, start with default and merge new
+    // viewport options later
+    if (enabledElement.viewport === undefined) {
+        enabledElement.viewport = getDefaultViewport(enabledElement.canvas);
     }
-  }
 
+    // Merge viewport
+    if (viewport) {
+        for (const attrname in viewport) {
+            if (viewport[attrname] !== null) {
+                enabledElement.viewport[attrname] = viewport[attrname];
+            }
+        }
+    }
 
-  // Prevent window width from being too small (note that values close to zero are valid and can occur with
-  // PET images in particular)
-  if (enabledElement.viewport.voi.windowWidth) {
-    enabledElement.viewport.voi.windowWidth = Math.max(viewport.voi.windowWidth, MIN_WINDOW_WIDTH);
-  }
+    // Prevent window width from being too small (note that values close to zero are valid and can occur with
+    // PET images in particular)
+    if (enabledElement.viewport.voi.windowWidth) {
+        enabledElement.viewport.voi.windowWidth = Math.max(
+            viewport.voi.windowWidth,
+            MIN_WINDOW_WIDTH
+        );
+    }
 
-  // Prevent scale from getting too small
-  if (enabledElement.viewport.scale) {
-    enabledElement.viewport.scale = Math.max(viewport.scale, MIN_VIEWPORT_SCALE);
-  }
+    // Prevent scale from getting too small
+    if (enabledElement.viewport.scale) {
+        enabledElement.viewport.scale = Math.max(
+            viewport.scale,
+            MIN_VIEWPORT_SCALE
+        );
+    }
 
-  // Normalize the rotation value to a positive rotation in degrees
-  enabledElement.viewport.rotation %= 360;
-  if (enabledElement.viewport.rotation < 0) {
-    enabledElement.viewport.rotation += 360;
-  }
+    // Normalize the rotation value to a positive rotation in degrees
+    enabledElement.viewport.rotation %= 360;
+    if (enabledElement.viewport.rotation < 0) {
+        enabledElement.viewport.rotation += 360;
+    }
 
-  if (enabledElement.image) {
-    // Force the image to be updated since the viewport has been modified
-    updateImage(element);
-  }
+    if (enabledElement.image) {
+        // Force the image to be updated since the viewport has been modified
+        updateImage(element);
+    }
 }
